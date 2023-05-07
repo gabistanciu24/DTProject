e935a8d112b8830d1edee4036ab6031395496e23
Stanciu Gabriel <gabistanciu24@yahoo.com>
2023-05-07 12:45:40 +0300

    Add VueRouter, routes to the main page, and link LoginPage.

--- healthx/src/main.js
+++ healthx/src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import { createRouter, createWebHistory } from "vue-router";
+import LoginPage from "./components/LoginPage.vue";
+import SignupPage from "./components/SignupPage.vue";
+
+const routes = [
+  {
+    path: "/login",
+    name: "login",
+    component: LoginPage,
+  },
+  {
+    path: "/",
+    name: "signup",
+    component: SignupPage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
 
 createApp(App).use(router).mount("#app");
